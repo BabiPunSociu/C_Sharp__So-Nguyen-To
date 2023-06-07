78af13de9324c4992b7974ce7b3edb3c7c732739
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:57:46 +0700

    DungNguyen

---
+++ KiemTra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoNguyenTo
+{
+    internal class KiemTra
+    {
+        public static bool check(int a)
+        {
+            if(a <= 1) return false;
+            for(int i=2;i <= Math.Sqrt(a);i++)
+            {
+                if(a%i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoNguyenTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            do
+            {
+                Console.Write("Nhap so nguyen duong a (a>1): a = ");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a <= 1);
+            
+            Console.WriteLine();
+            Console.WriteLine(KiemTra.check(a));
+        }
+    }
+}
